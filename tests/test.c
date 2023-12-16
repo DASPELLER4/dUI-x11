147c7979647ce769e81ae3ce6f62931b0696abab
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2023-12-16 20:42:19 +0000

    updated test

--- tests/test.c
+++ tests/test.c
@@ -1,34 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 #include "../include/dui.h"
 
 void a(){
-	printf("clicked\n");
+	printf("Clicked Button\n");
 }
 
-input_t *input;
-
 void b(){
-	printf("entered %s\n", input->input);
+	printf("Clicked Label\n");
+}
+
+void c(){
+	printf("Clicked Input\n");
+}
+
+int x = 0;
+
+void d(){
+	x++;
+	printf("Hovered Over Label For %d Frames\n",x);
+}
+
+void e(char c){
+	printf("%c\n", c);
+}
+
+uiElement_t *input;
+uiElement_t *button;
+screen_t *screen;
+
+void f(){
+	setElementText(getElementText(input), button, screen);
 }
 
 int main(int argc, char **argv){
-	screen_t *screen = createScreen(0,0,1920,1080);
-	text_t *text = createTextElement(700,100,"text",10,ORANGE,YELLOW,screen->bpp);
-	button_t *button = createButtonElement(0, 0, "a",7,BLUE,GREEN,a,screen->bpp);
-	text_t *text2 = createTextElement(0,92,"a",TEXT_S,WHITE,WHITE,screen->bpp);
-	input = createInputElement(90, 50, 20, 3, LT_GREEN, DK_GREEN, screen->bpp);
-	input->onReturn = b;
-	addButton(button, screen);
-	addInput(input, screen);
-	addText(text, screen);
-	addText(text2, screen);
-	printf("Bytes Per Pixel : %d\n",screen->bpp);
+	screen = createScreen(0,0,1000,1000);
+	uiElement_t *label = createLabel(0,0,"Label",TEXT_M,LT_RED,DK_RED,screen->display);
+	button = createButton(0,label->generic.pxheight,"Button",TEXT_M,LT_BLUE,DK_BLUE,screen->display);
+	input = createInput(0,button->generic.pxheight+button->generic.y,8,TEXT_L,LT_GREEN,DK_GREEN,screen->display);
+	uiElement_t *followLabel = createLabel(0,0,"cursor", TEXT_S, LT_YELLOW, DK_YELLOW, screen->display);
+	addElement(label, screen);
+	addElement(button, screen);
+	addElement(input, screen);
+	addElement(followLabel, screen);
+	setOnClick(input, c);
+	setOnClick(label, b);
+	setOnClick(button, a);
+	setOnHover(label, d);
+	setOnKeyPress(input, e);
+	setOnReturn(input, f);
+	clock_t begin = clock();
+	int frames = 0;
 	while(screen->keyboard->keypressed != 'q'){
-		printf("%ld\n", sizeof(*input));
-		text2->x = screen->mouse->x;
-		text2->y = screen->mouse->y;
+		moveElement(screen->mouse->x, screen->mouse->y, followLabel, screen);
 		renderScreen(screen);
+		frames++;
 	}
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	double fps = ((double)frames)/time_spent;
+	printf("FPS - %f\n", fps);
 	closeScreen(screen);
 }
+
